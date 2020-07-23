1736c9673e6b414602c7513aab636cc8bba2801c
Trung Pham <phamanhtrung@gmail.com>
2020-07-23 11:57:42 -0500

    add event handler to authentication form

--- CO_Front_End/carrieokey/src/App.js
+++ CO_Front_End/carrieokey/src/App.js
@@ -6,7 +6,45 @@ import About from "./components/About";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 
+const baseUrl = "http://localhost:3003";
+
 export default class App extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  handleSignup = (event) => {
+    console.log("handle signup clicked");
+    event.preventDefault();
+    fetch(baseUrl + "/users", {
+      method: "POST",
+      body: JSON.stringify({
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        email: this.state.email,
+        password: this.state.password,
+      }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
   render() {
     return (
       <Router>
@@ -16,7 +54,20 @@ export default class App extends Component {
             <Route exact path="/" component={Home} />
             <Route exact path="/about" component={About} />
             <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
+            <Route
+              exact
+              path="/signup"
+              render={() => (
+                <Signup
+                  firstName={this.state.firstName}
+                  lastName={this.state.lastName}
+                  email={this.state.email}
+                  password={this.state.password}
+                  handleChange={this.handleChange}
+                  handleSignup={this.handleSignup}
+                />
+              )}
+            />
           </Switch>
         </div>
       </Router>
