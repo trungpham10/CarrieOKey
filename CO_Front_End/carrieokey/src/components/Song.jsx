02479e223db9a7d05553c35222c669f02f834f16
Joshua Agee <joshua.agee@protonmail.com>
2020-07-24 07:19:58 -0500

    formatting song component as cards for list

--- CO_Front_End/carrieokey/src/components/Song.jsx
+++ CO_Front_End/carrieokey/src/components/Song.jsx
@@ -1,16 +1,23 @@
 import React, { Component } from 'react'
 
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 export default class Song extends Component {
     render() {
         return (
-            <div>
-                <img src={this.props.image} />
-                <h2>{this.props.songName}</h2>
-                <h3>{this.props.artist}</h3>
-                <p>{this.props.lyrics}</p>
-                <a href={this.props.videoLink}></a>
-                <br></br>
-            </div>
+            <Card className='text-center bg-dark text-white' style={{width: '20rem'}}>
+                <Card.Img variant='top' src={this.props.image} alt="album cover" />
+                <Card.Body>
+                <Card.Title>{this.props.songName}</Card.Title>
+                <Card.Subtitle>
+                By {this.props.artist}
+                </Card.Subtitle>
+                <Card.Text>
+                {this.props.lyrics}
+                </Card.Text>
+                <a href={this.props.videoLink}><Button variant="primary">Link to Video of this Song</Button></a>
+                </Card.Body>
+            </Card>
         )
     }
 }
