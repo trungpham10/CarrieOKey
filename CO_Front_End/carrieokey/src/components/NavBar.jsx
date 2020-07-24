a564186c1053c45e14dcf6a4e939ff5f33607cfe
Trung Pham <phamanhtrung@gmail.com>
2020-07-24 14:29:51 -0500

    separate authentication button in navigation bar

--- CO_Front_End/carrieokey/src/components/NavBar.jsx
+++ CO_Front_End/carrieokey/src/components/NavBar.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import "../App.css";
 
-export default function NavBar(props) {
-  if (props.isLoggedIn) {
-    console.log("flag");
-  }
-
+export default function NavBar() {
   return (
     <Container>
       <Navbar sticky="top">
@@ -19,20 +16,26 @@ export default function NavBar(props) {
             <Link to="/about">About</Link>
           </li>
           <li>
-            <Link to="/login">Log in</Link>
-          </li>
-          <li>
-            <Link to="/signup">Sign up</Link>
-          </li>
-          <li>
-            <Link to="/login">Log out</Link>
-          </li>
-          <li>
             <Link to="/newSong">New Song</Link>
           </li>
           <li>
             <Link to="/songs">Song List</Link>
           </li>
+        </ul>
+
+        <ul className="authentication-button">
+          <button>
+            <Link to="/login">Log in</Link>
+          </button>
+          <br />
+          <button>
+            <Link to="/signup">Sign up</Link>
+          </button>
+          <br />
+          <button>
+            <Link to="/login">Log out</Link>
+          </button>
+          <br />
         </ul>
       </Navbar>
     </Container>
