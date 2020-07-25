f2861b444f50025d2751510c97ca5e505b6335bc
Joshua Agee <joshua.agee@protonmail.com>
2020-07-25 12:28:39 -0500

    navbar changes and button formating

--- CO_Front_End/carrieokey/src/components/NavBar.jsx
+++ CO_Front_End/carrieokey/src/components/NavBar.jsx
@@ -1,39 +1,60 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import "../App.css";
 import AuthenticationNav from "./AuthenticationNav";
+import Button from 'react-bootstrap/Button'
 
 export default function NavBar(props) {
   return (
     <Container>
-      <Navbar sticky="top">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/newSong">New Song</Link>
-          </li>
-          <li>
-            <Link to="/songs">Song List</Link>
-          </li>
-          <li>
-            <Link to="/video">Karaoke room</Link>
-          </li>
-        </ul>
+      <Nav sticky="top">
 
-        <ul className="authentication-button">
-          <AuthenticationNav
+        <Nav.Item>
+          <Nav.Link><Link to="/">Home</Link></Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link><Link to="/about">About</Link></Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link><Link to="/newSong">New Song</Link></Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link><Link to="/songs">Song List</Link></Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link><Link to="/video">Karaoke room</Link></Nav.Link>
+        </Nav.Item>
+        {/* //conditional rendering of buttons based on logged in state */}
+        {props.isLoggedIn ?
+          <Nav.Item>
+            <Nav.Link><Link to="/login" onClick={() => props.handleLogout()}>
+              <Button>Log out</Button>
+            </Link></Nav.Link>
+          </Nav.Item> 
+          :
+          <>
+            <Nav.Item>
+              <Nav.Link > <Link to="/login">
+                <Button>Log in</Button>
+              </Link></Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link > <Link to="/signup">
+                <Button>Sign up</Button>
+              </Link></Nav.Link>
+            </Nav.Item>
+          </>
+        }
+
+
+        {/* <AuthenticationNav
             isLoggedIn={props.isLoggedIn}
             handleLogout={props.handleLogout}
-          />
-        </ul>
-      </Navbar>
+          /> */}
+
+      </Nav>
     </Container>
   );
 }
