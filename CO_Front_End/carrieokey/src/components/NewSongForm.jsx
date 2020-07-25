15cb2f4d851206b477e26bf362ffe7318e2aaefc
Joshua Agee <joshua.agee@protonmail.com>
2020-07-25 11:08:02 -0500

    fix bug with new song form

--- CO_Front_End/carrieokey/src/components/NewSongForm.jsx
+++ CO_Front_End/carrieokey/src/components/NewSongForm.jsx
@@ -4,52 +4,52 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form'
 const baseUrl = 'http://localhost:3003'
 export default class NewSongForm extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             songName: "",
             artist: "",
             lyrics: "",
             videoLink: "",
-            image: ""  
+            image: ""
         }
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
 
 
     }
-    
+
     handleChange(event) {
-        this.setState({[event.currentTarget.id]: event.currentTarget.value})
+        this.setState({ [event.currentTarget.id]: event.currentTarget.value })
     }
 
-handleSubmit (event) {
-    event.preventDefault()
-    fetch(baseUrl + '/song', {
-        method: 'POST',
-        body: JSON.stringify({
-            songName: this.state.songName,
-            artist: this.state.artist,
-            lyrics: this.state.lyrics,
-            videoLink: this.state.videoLink,
-            image: this.state.image
-        }),
-        headers: {
-        'Content-Type': 'application/json'
-        }
-    }).then (res => res.json())
-        .then (resJson => {
-            console.log(resJson)
-        // this.props.handleAddSong(resJson)
-        this.setState({
-            songName: '',
-            artist: '',
-            lyrics: '',
-            videoLink: '',
-            image: ''
-        })
-    }).catch (error => console.error({'Error': error}))
-}
+    handleSubmit(event) {
+        event.preventDefault()
+        fetch(baseUrl + '/song', {
+            method: 'POST',
+            body: JSON.stringify({
+                songName: this.state.songName,
+                artist: this.state.artist,
+                lyrics: this.state.lyrics,
+                videoLink: this.state.videoLink,
+                image: this.state.image
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json())
+            .then(resJson => {
+                console.log(resJson)
+                // this.props.handleAddSong(resJson)
+                this.setState({
+                    songName: '',
+                    artist: '',
+                    lyrics: '',
+                    videoLink: '',
+                    image: ''
+                })
+            }).catch(error => console.error({ 'Error': error }))
+    }
 
 
 
@@ -57,28 +57,30 @@ handleSubmit (event) {
     render() {
         return (
             <Container>
-            <Form.Group onSubmit={(event) => this.handleSubmit(event)}>
+                <Form onSubmit={(event) => this.handleSubmit(event)}>
+                <Form.Group >
 
                     <Form.Control type="text" placeholder="Song Title" id="songName" value={this.state.songName} onChange={this.handleChange} />
                     <br />
-            
+
 
                     <Form.Control type="text" placeholder="Artist" id="artist" value={this.state.artist} onChange={this.handleChange} />
-                    <br/>
-
-                    <Form.Group controlId="Form.ControlTextarea"/>
-                    <Form.Control as="textarea" rows="2" placeholder="Lyrics" id="lyrics" value={this.state.lyrics} onChange={this.handleChange}/><br/>
-            
-                    <Form.Control type="text" placeholder="Video Link" id="videoLink" value={this.state.videoLink} onChange={this.handleChange}/>
                     <br />
-            
 
-                    <Form.Control type="text" id="image" placeholder="Image Link"  value={this.state.image} onChange={this.handleChange}/><br></br>
-            
+                    <Form.Group controlId="Form.ControlTextarea" />
+                    <Form.Control as="textarea" rows="2" placeholder="Lyrics" id="lyrics" value={this.state.lyrics} onChange={this.handleChange} /><br />
 
-                <Button type="submit"variant="warning">Add Song</Button>
+                    <Form.Control type="text" placeholder="Video Link" id="videoLink" value={this.state.videoLink} onChange={this.handleChange} />
+                    <br />
 
-            </Form.Group>
+
+                    <Form.Control type="text" id="image" placeholder="Image Link" value={this.state.image} onChange={this.handleChange} /><br></br>
+
+
+                    <Button type="submit" variant="warning">Add Song</Button>
+
+                </Form.Group>
+                </Form>
             </Container>
         )
     }
