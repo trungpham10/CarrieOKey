17644a33e63185a9859c850c7f6492c817458496
Trung Pham <phamanhtrung@gmail.com>
2020-07-25 12:18:01 -0500

    fix lyrics display format

--- CO_Front_End/carrieokey/src/components/Song.jsx
+++ CO_Front_End/carrieokey/src/components/Song.jsx
@@ -1,27 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 export default class Song extends Component {
-    render() {
-        return (
-            
-            <Card className='text-center bg-dark text-white' style={{width: '20rem'}}>
-                <Card.Img variant='top' src={this.props.image} alt="album cover" />
-                <Card.Body>
-                <Card.Title>{this.props.songName}</Card.Title>
-                <Card.Subtitle>
-                By {this.props.artist}
-                </Card.Subtitle>
-                <Card.Text>
-                {this.props.lyrics.slice(1, this.props.lyricLength)}
-                </Card.Text>
-                <a href={this.props.videoLink}><Button variant="primary">Link to Video of this Song</Button></a>
-                <Button variant="danger" onClick={()=> this.props.deleteSong(this.props.songID)}>Delete </Button>
-                </Card.Body>
-            </Card>
-            
-        )
-    }
+  render() {
+    return (
+      <Card
+        className="text-center bg-dark text-white"
+        style={{ width: "20rem" }}
+      >
+        <Card.Img variant="top" src={this.props.image} alt="album cover" />
+        <Card.Body>
+          <Card.Title>{this.props.songName}</Card.Title>
+          <Card.Subtitle>By {this.props.artist}</Card.Subtitle>
+          <Card.Text>
+            <pre>{this.props.lyrics.slice(1, this.props.lyricLength)}</pre>
+          </Card.Text>
+          <a href={this.props.videoLink}>
+            <Button variant="primary">Link to Video of this Song</Button>
+          </a>
+          <Button
+            variant="danger"
+            onClick={() => this.props.deleteSong(this.props.songID)}
+          >
+            Delete{" "}
+          </Button>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
