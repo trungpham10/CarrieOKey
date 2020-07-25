2a8050c6d5ace686a136ddc609b3df992d8ad949
Trung Pham <phamanhtrung@gmail.com>
2020-07-24 21:41:58 -0500

    display song list in login page

--- CO_Front_End/carrieokey/src/components/Login.jsx
+++ CO_Front_End/carrieokey/src/components/Login.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "../App.css";
 import { Redirect } from "react-router-dom";
+import SongList from "./SongList";
+import Container from "react-bootstrap/Container";
 
 export default function Login(props) {
   if (props.isLoggedIn) {
@@ -8,29 +10,33 @@ export default function Login(props) {
   }
 
   return (
-    <div className="log-in">
-      <h2>Log in</h2>
-      <form className="login-form" onSubmit={(evt) => props.handleLogin(evt)}>
-        <input
-          type="text"
-          id="logEmail"
-          name="email"
-          placeholder="Email"
-          onChange={(evt) => props.handleChange(evt)}
-          value={props.logEmail}
-        ></input>
-        <br />
-        <input
-          type="password"
-          id="logPassword"
-          name="password"
-          placeholder="Password"
-          onChange={(evt) => props.handleChange(evt)}
-          value={props.logPassword}
-        ></input>
-        <br />
-        <input type="submit" id="login-submit" value="Log in" />
-      </form>
-    </div>
+    <Container>
+      <div className="log-in">
+        <h2>Log in</h2>
+        <form className="login-form" onSubmit={(evt) => props.handleLogin(evt)}>
+          <input
+            type="text"
+            id="logEmail"
+            name="email"
+            placeholder="Email"
+            onChange={(evt) => props.handleChange(evt)}
+            value={props.logEmail}
+          ></input>
+          <br />
+          <input
+            type="password"
+            id="logPassword"
+            name="password"
+            placeholder="Password"
+            onChange={(evt) => props.handleChange(evt)}
+            value={props.logPassword}
+          ></input>
+          <br />
+          <input type="submit" id="login-submit" value="Log in" />
+        </form>
+      </div>
+      <br />
+      <SongList />
+    </Container>
   );
 }
