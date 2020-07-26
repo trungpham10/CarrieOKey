ca9b361bc9011f2d5a8ac051799a4b00fc3bc790
Naomi <naomi@macbook-pro.attlocal.net>
2020-07-26 14:09:23 -0500

    cards sized

--- CO_Front_End/carrieokey/src/components/Song.jsx
+++ CO_Front_End/carrieokey/src/components/Song.jsx
@@ -2,63 +2,32 @@ import React, { Component } from "react";
 
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/esm/Container";
 
 export default class Song extends Component {
-  render() {
-    return (
-      <Card
-        className="text-left bg-dark text-white mb-2"
-        style={{
-          width: "65rem",
-          height: "30rem",
-          display: "flex",
-          flexDirection: "row",
-        }}
-      >
-        <Card.Img
-          variant="top"
-          src={this.props.image}
-          alt="album cover"
-          style={{ width: "20rem", height: "20rem", alignContent: "center" }}
-        />
-        <Card.Body>
-          <Card.Title>
-            <h1>{this.props.songName}</h1>
-          </Card.Title>
-          <Card.Subtitle>
-            <p>{this.props.artist}</p>
-          </Card.Subtitle>
-          <br />
-          <Card.Text>
-            <pre className="text-white text-left">
-              {this.props.lyrics.slice(0, this.props.lyricLength)}...
-            </pre>
-          </Card.Text>
-          {this.props.videoLink ? (
-            <a href={this.props.videoLink}>
-              <Button variant="primary">Link to Video of this Song</Button>
-            </a>
-          ) : (
-            ""
-          )}
-          <Container
-            className="text-bottom bottom-buttons"
-            variant="bottom"
-            style={{ marginBottom: "0" }}
-          >
-            {/* <Card.Link href="/songs">More</Card.Link>
-            <br />
-            <Button>Karaoke!</Button> */}
-            {/* <Button
-              variant="danger"
-              onClick={() => this.props.deleteSong(this.props.songID)}
-            >
-              Delete{" "}
-            </Button> */}
-          </Container>
-        </Card.Body>
-      </Card>
-    );
-  }
+    render() {
+        return (
+            
+            <Card className='text-center bg-dark text-white mb-2 ' style={{width: '20rem'}} id="cardStyle">
+                <Card.Img variant='top' src={this.props.image} alt="album cover" />
+                <Card.Body>
+                <Card.Title>{this.props.songName}</Card.Title>
+                <Card.Subtitle>
+                By {this.props.artist}
+                </Card.Subtitle>
+                <Card.Text>
+                    {/* <pre className="text-white">
+                        {this.props.lyrics.slice(1, this.props.lyricLength)}
+                    </pre> */}
+                </Card.Text>{
+                    this.props.videoLink ?
+                    <a href={this.props.videoLink}><Button variant="primary">Link to Video of this Song</Button></a>
+                    :
+                    ""
+                }
+                <Button variant="danger" onClick={()=> this.props.deleteSong(this.props.songID)}>Delete </Button>
+                </Card.Body>
+            </Card>
+            
+        )
+    }
 }
