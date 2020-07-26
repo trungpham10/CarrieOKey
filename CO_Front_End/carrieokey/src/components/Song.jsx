c441ea8e28d6612132f210bdad0336e67b851344
Naomi <naomi@macbook-pro.attlocal.net>
2020-07-26 15:13:50 -0500

    click card redirect to show

--- CO_Front_End/carrieokey/src/components/Song.jsx
+++ CO_Front_End/carrieokey/src/components/Song.jsx
@@ -4,30 +4,39 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
 export default class Song extends Component {
-    render() {
-        return (
-            
-            <Card className='text-center bg-dark text-white mb-2 ' style={{width: '20rem'}} id="cardStyle">
-                <Card.Img variant='top' src={this.props.image} alt="album cover" />
-                <Card.Body>
-                <Card.Title>{this.props.songName}</Card.Title>
-                <Card.Subtitle>
-                By {this.props.artist}
-                </Card.Subtitle>
-                <Card.Text>
-                    {/* <pre className="text-white">
+  render() {
+    return (
+      <a href="./Show.jsx">
+        <Card
+          className="text-center bg-dark text-white mb-2 "
+          style={{ width: "20rem" }}
+          id="cardStyle"
+        >
+          <Card.Img variant="top" src={this.props.image} alt="album cover" />
+          <Card.Body>
+            <Card.Title>{this.props.songName}</Card.Title>
+            <Card.Subtitle>By {this.props.artist}</Card.Subtitle>
+            <Card.Text>
+              {/* <pre className="text-white">
                         {this.props.lyrics.slice(1, this.props.lyricLength)}
                     </pre> */}
-                </Card.Text>{
-                    this.props.videoLink ?
-                    <a href={this.props.videoLink}><Button variant="primary">Link to Video of this Song</Button></a>
-                    :
-                    ""
-                }
-                <Button variant="danger" onClick={()=> this.props.deleteSong(this.props.songID)}>Delete </Button>
-                </Card.Body>
-            </Card>
-            
-        )
-    }
+            </Card.Text>
+            {this.props.videoLink ? (
+              <a href={this.props.videoLink}>
+                <Button variant="primary">Link to Video of this Song</Button>
+              </a>
+            ) : (
+              ""
+            )}
+            <Button
+              variant="danger"
+              onClick={() => this.props.deleteSong(this.props.songID)}
+            >
+              Delete{" "}
+            </Button>
+          </Card.Body>
+      </Card>
+    </a>
+    );
+  }
 }
