0c5bbb2107ebbb758e0f6b2cb216fca32004af38
Trung Pham <phamanhtrung@gmail.com>
2020-07-25 22:09:04 -0500

    redesign songs layout

--- CO_Front_End/carrieokey/src/components/Song.jsx
+++ CO_Front_End/carrieokey/src/components/Song.jsx
@@ -2,22 +2,37 @@ import React, { Component } from "react";
 
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/esm/Container";
 
 export default class Song extends Component {
   render() {
     return (
       <Card
-        className="text-center bg-dark text-white mb-2"
-        style={{ width: "20rem", height: "50rem" }}
+        className="text-left bg-dark text-white mb-2"
+        style={{
+          width: "65rem",
+          height: "30rem",
+          display: "flex",
+          flexDirection: "row",
+        }}
       >
-        <Card.Img variant="top" src={this.props.image} alt="album cover" />
+        <Card.Img
+          variant="top"
+          src={this.props.image}
+          alt="album cover"
+          style={{ width: "20rem", height: "20rem", alignContent: "center" }}
+        />
         <Card.Body>
-          <Card.Title>{this.props.songName}</Card.Title>
-          <Card.Subtitle>By {this.props.artist}</Card.Subtitle>
+          <Card.Title>
+            <h1>{this.props.songName}</h1>
+          </Card.Title>
+          <Card.Subtitle>
+            <p>{this.props.artist}</p>
+          </Card.Subtitle>
           <br />
           <Card.Text>
             <pre className="text-white text-left">
-              {this.props.lyrics.slice(0, this.props.lyricLength)}
+              {this.props.lyrics.slice(0, this.props.lyricLength)}...
             </pre>
           </Card.Text>
           {this.props.videoLink ? (
@@ -27,15 +42,21 @@ export default class Song extends Component {
           ) : (
             ""
           )}
-          <a href="/songs/:id">More</a>
-          <br />
-          <Button>Karaoke!</Button>
-          <Button
-            variant="danger"
-            onClick={() => this.props.deleteSong(this.props.songID)}
+          <Container
+            className="text-bottom bottom-buttons"
+            variant="bottom"
+            style={{ marginBottom: "0" }}
           >
-            Delete{" "}
-          </Button>
+            {/* <Card.Link href="/songs">More</Card.Link>
+            <br />
+            <Button>Karaoke!</Button> */}
+            {/* <Button
+              variant="danger"
+              onClick={() => this.props.deleteSong(this.props.songID)}
+            >
+              Delete{" "}
+            </Button> */}
+          </Container>
         </Card.Body>
       </Card>
     );
